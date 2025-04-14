a956f7adb9926e821f6ef91863c52b6647bab975
abagatov <a.bagatov@norsi-trans.ru>
2025-04-14 19:44:01 +0300

    Смех

--- app.vue
+++ app.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
     <p>test test привет</p>
-    <p>CI/CD</p>
-    <p>CI/CD</p>
+    <p class="vibrate">CI/CD</p>
+    <p class="vibrate">CI/CD</p>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
 </template>
+
+<style scoped>
+.vibrate {
+  display: inline-block;
+  animation: vibrate 0.5s linear infinite;
+}
+
+@keyframes vibrate {
+  0% { transform: translateX(0); }
+  25% { transform: translateX(-5px); }
+  50% { transform: translateX(5px); }
+  75% { transform: translateX(-5px); }
+  100% { transform: translateX(0); }
+}
+</style>
